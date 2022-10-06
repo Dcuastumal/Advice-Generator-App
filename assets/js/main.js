4dd5068b57b3b8017de848dbdbbe89c06cadc703
David Alejandro Cuastumal Bucheli <79026475+Dcuastumal@users.noreply.github.com>
2022-10-06 14:14:45 -0500

    final commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const api = "https://api.adviceslip.com/advice";
+
+async function loadRandomAdvice() {
+    const response = await fetch(api);
+    const data = await response.json();
+
+    if (response.status !== 200) {
+        spanError.innerHTML = "Hubo un error" + response.status;
+    } else {
+        const idNumber = document.getElementById('id-number');
+        const apiAdvice = document.getElementById('span-random-advices');
+
+        idNumber.innerHTML = data.slip.id;
+        apiAdvice.innerHTML = data.slip.advice;
+    }
+}
+
+loadRandomAdvice();
